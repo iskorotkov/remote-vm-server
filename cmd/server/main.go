a195de029a416f1eb6b759bdf7594e0b9049f15d
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2021-09-12 09:09:56 +0500

    feat: implement auth and callback handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,105 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+)
+
+var (
+	Host         = os.Getenv("HOST")
+	ClientID     = os.Getenv("CLIENT_ID")
+	ClientSecret = os.Getenv("CLIENT_SECRET")
+	RedirectURL  = os.Getenv("REDIRECT_URL")
 )
 
 func main() {
+	startHTTPServer()
+}
+
+func startHTTPServer() {
 	r := mux.NewRouter()
-
 	r.Use(logRequests)
-
-	r.HandleFunc("/api/v1/do/signup", signup).Methods("POST")
-	r.HandleFunc("/api/v1/do/callback", callback).Methods("POST")
+	r.HandleFunc("/api/v1/auth", auth).Methods("POST")
+	r.HandleFunc("/api/v1/callback", callback).Methods("POST")
 
 	log.Println("service started listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
+func auth(w http.ResponseWriter, r *http.Request) {
+	config := oauth2.Config{
+		ClientID:     ClientID,
+		ClientSecret: ClientSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  fmt.Sprintf("%s/v1/oauth/authorize", Host),
+			TokenURL: fmt.Sprintf("%s/v1/oauth/token", Host),
+		},
+		RedirectURL: RedirectURL,
+		Scopes:      []string{"read", "write"},
+	}
 
+	// TODO: Generate and store state.
+	url := config.AuthCodeURL("state")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
 func callback(w http.ResponseWriter, r *http.Request) {
+	config := oauth2.Config{
+		ClientID:     ClientID,
+		ClientSecret: ClientSecret,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  fmt.Sprintf("%s/v1/oauth/authorize", Host),
+			TokenURL: fmt.Sprintf("%s/v1/oauth/token", Host),
+		},
+		RedirectURL: RedirectURL,
+		Scopes:      []string{"read", "write"},
+	}
 
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("error reading request body: %v", err)
+		http.Error(w, "error reading request body", http.StatusBadRequest)
+
+		return
+	}
+
+	var body struct {
+		Code string `json:"code"`
+	}
+
+	err = json.Unmarshal(b, &body)
+	if err != nil {
+		log.Printf("error parsing request body: %v", err)
+		http.Error(w, "error parsing request body", http.StatusBadRequest)
+
+		return
+	}
+
+	token, err := config.Exchange(context.Background(), body.Code)
+	if err != nil {
+		log.Printf("error exchanging code for tokens: %v", err)
+		http.Error(w, "error exchanging code for tokens", http.StatusInternalServerError)
+
+		return
+	}
+
+	b, err = json.Marshal(token)
+	if err != nil {
+		log.Printf("error serializing token: %v", err)
+		http.Error(w, "error serializing token", http.StatusInternalServerError)
+
+		return
+	}
+
+	_, _ = fmt.Fprint(w, b)
 }
 
 func logRequests(h http.Handler) http.Handler {
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/iskorotkov/remote-vm-server
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/gorilla/mux v1.8.0
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
